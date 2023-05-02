168c8efa8775b9c5252fc05d7588f911df614d78
Kumar Abhishek <kumar.abhishek@i-exceed.com>
2023-05-02 11:17:54 +0530

    added extension function to show toast. added toast in EditProfileStudent.kt

--- app/src/main/java/com/example/myhometutor/STUDENTS/EditProfileStudent.kt
+++ app/src/main/java/com/example/myhometutor/STUDENTS/EditProfileStudent.kt
@@ -3,10 +3,13 @@ package com.example.myhometutor.STUDENTS
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.myhometutor.R
+import com.example.myhometutor.UTILS.showToast
 
 class EditProfileStudent : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_profile_student)
+
+        showToast("Test Toast")
     }
 }
